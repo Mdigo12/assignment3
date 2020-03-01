033f2a1fed3fb1b68c3ac57db1e46c2b401bafb4
Mdigo12 <mdigojacob@gmail.com>
2020-03-01 06:57:11 +0300

    created a business logic in js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Business logic
+
+var radisson=Radisson_Blu_Anchorage_Hotel_Lagos_5_4-6;
+var lagos=LagosContinentalHotelLagos6_8-10 ;
+var villa=VillaRosaKempinskiNairobi6_4-6 ;
+var  fairmont=HotelFairmont_The_Norfolk_Nairobi_5_8-10;
+var taj=TajCapeTown_5_4-6;
+var only=One&OnlyCapeTown_6_8-10;
+var nile=TheNileRitz-CarltonCairo_5_4-6;
+var kemp=KempinskiNileHotelGardenCityCairo_6_8-10;
+
+// user logic
